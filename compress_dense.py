6c7c72df5d67fe4d5157236a3bed6d8e0fd6923d
Francois Bourassa <francois.bourassa4@mail.mcgill.ca>
2019-05-08 02:47:36 -0400

    Trying to convert to sparse by blocks

--- compress_dense.py
+++ compress_dense.py
@@ -4,6 +4,24 @@ from time import time
 import scipy as sp
 from scipy import sparse
 
+
+def convert_by_chunks(df, chunksize=500, dty=np.int16):
+    blocks = []
+    # Drop one block of columns at a time from the dataframe
+    for i in range(df.shape[1]//chunksize):
+        chunk = df.values[:, :chunksize]
+        blocks.append(sp.sparse.csc_matrix(chunk, dtype=dty))
+        df.drop(labels=range(i*chunksize, (i+1)*chunksize), axis=1, inplace=True)
+        print("Chunk {} done".format(i))
+
+    # remaining part: rename columns so we know hwere to start
+    df.columns=range(df.shape[1])
+    blocks.append(sp.sparse.csc_matrix(df.values, dtype=dty))
+    df.drop(labels=range(df.shape[1]), inplace=True)
+
+    # Put the blocks together
+    return sp.sparse.hstack(blocks, dtype=dty)
+
 if __name__ == "__main__":
     fname = "data/GSE102827/GSE102827_frame_formatted_sparse.pkl"
     new_f = "data/GSE102827/GSE102827_frame_values_sparse.npz"
@@ -18,19 +36,19 @@ if __name__ == "__main__":
     print("DataFrame memory usage:", df.memory_usage().sum()/1024**2, "MB")
 
     # Save the values to a sparse matrix, save the indexes separately
-    idx, cols = df.index, df.columns
-    vals = df.values
-    dty = vals.dtype
-    del df  # need to free memory, can't copy everything
-    vals = sp.sparse.csc_matrix(vals, dtype=dty)
     inter2 = time()
+    save_object(idx, "data/GSE102827/GSE102827_frame_index.pkl")
+    save_object(cols, "data/GSE102827/GSE102827_frame_columns.pkl")
+    df.reset_index(drop=True, inplace=True)
+    df.columns = range(df.shape[1])  # takes up less space
+    dty = np.int16
+    vals = convert_by_chunks(df, chunksize=5000, dty=dty)
+
     print("Converted to sparse in {} s".format(inter2 - inter1))
     size = vals.data.nbytes + vals.indices.nbytes + vals.indptr.nbytes
     print('{:.2f} MB of data'.format(size / 1024 ** 2))
 
-    # Save as a hdf file for faster import later on
+    # Save as a npz file for faster import later on
     sp.sparse.save_npz(new_f, vals)
-    save_object(idx, "data/GSE102827/GSE102827_frame_index.pkl")
-    save_object(cols, "data/GSE102827/GSE102827_frame_columns.pkl")
     final = time()
     print("Finished saving as npz in {} s".format(final - inter2))
--- compress_tests.py
+++ compress_tests.py
@@ -5,26 +5,47 @@ import numpy as np
 import scipy as sp
 from scipy import sparse
 
+def convert_by_chunks(df, chunksize=500, dty=np.int16):
+    blocks = []
+    # Drop one block of columns at a time from the dataframe
+    for i in range(df.shape[1]//chunksize):
+        chunk = df.values[:, :chunksize]
+        blocks.append(sp.sparse.csc_matrix(chunk, dtype=dty))
+        df.drop(labels=range(i*chunksize, (i+1)*chunksize), axis=1, inplace=True)
+        print("Chunk {} done".format(i))
+
+    # remaining part: rename columns so we know hwere to start
+    df.columns=range(df.shape[1])
+    blocks.append(sp.sparse.csc_matrix(df.values, dtype=dty))
+    df.drop(labels=range(df.shape[1]), inplace=True)
+
+    # Put the blocks together
+    return sp.sparse.hstack(blocks, dtype=dty)
+
 if __name__ == "__main__":
     new_f = "test.npz"
 
-    vals = np.random.rand(5000,5000)
+    vals = np.random.rand(5000,5005)
     vals[vals < 0.77] = 0.
     df = pd.DataFrame(vals)
+    save_object(df.index, "index_test.pkl")
+    save_object(df.columns, "columns_test.pkl")
+    df.reset_index(drop=True, inplace=True)
+    df.columns = range(df.shape[1])  # takes up less space
     print("Dense memory usage:", df.memory_usage().sum()/1024**2, "MB")
 
     # Convert to sparse
     start = time()
-    vals = sp.sparse.csc_matrix(df.values, dtype=df.values.dtype)
+    vals = convert_by_chunks(df)
+    print(df.shape)
+    del df
     inter1 = time()
     print("Converted to sparse in {} s".format(inter1 - start))
-    print("Sparse memory usage:", df.memory_usage().sum()/1024**2, "MB")
-
+    size = vals.data.nbytes + vals.indices.nbytes + vals.indptr.nbytes
+    print('{:.2f} MB of data'.format(size / 1024 ** 2))
 
     # Save as a hdf file for faster import later on
     #df.to_hdf(new_f, key='df', mode='w', compression=9, complib='blosc')
     sp.sparse.save_npz(new_f, vals)
-    save_object(df.index, "index_test.pkl")
-    save_object(df.columns, "columns_test.pkl")
     final = time()
-    print("Finished saving as HDF5 in {} s".format(final - inter1))
+    print("Finished saving as npz in {} s".format(final - inter1))
