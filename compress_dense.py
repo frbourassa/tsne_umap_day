c360ea871667d183f1f18f54e908f2841e169bc3
Francois Bourassa <francois.bourassa4@mail.mcgill.ca>
2019-05-07 23:56:20 -0400

    Fixed mistakes for compressing to sparse; using hdf5 format now

--- compress_dense.py
+++ compress_dense.py
@@ -3,7 +3,9 @@ from format_tools import load_object, save_object
 
 if __name__ == "__main__":
     fname = "data/GSE102827/GSE102827_frame_formatted_dense.pkl"
-    new_f = "data/GSE102827/GSE102827_frame_formatted_sparse.pkl"
+    new_f = "data/GSE102827/GSE102827_frame_formatted_sparse.h5"
     df = load_object(fname)
-    df.to_sparse()
-    save_object(df, new_f)
+    df = df.to_sparse(fill_value=0)
+
+    # Save as a hdf file for faster import later on
+    df.to_hdf(new_f, key='df', mode='w')
